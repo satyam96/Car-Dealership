5d44a6cca1be87c19ae408a3a58f026859580347
Nishank Bhati <nishank123xyz@gmail.com>
2016-10-18 10:49:54 +0530

    Create car_deal.c

---
+++ car_deal.c
@@ -0,0 +1,156 @@
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<stdbool.h>
+
+//Structure for D
+
+struct node{
+	int key;	//key=Car prices
+	int value;	//value=model
+	int height;	//AVL constraint :p 
+	struct node *left;
+	struct node *right;
+};
+
+int max(int a,int b)
+{
+	if(a>b) return a;
+	else return b;
+}
+
+int height(struct node *N){
+	if(N==NULL)
+		return 0;
+	else
+		return N->height;
+}
+
+struct node *newNode(int key,int value){
+	struct node* node=(struct node*)malloc(sizeof(struct node));
+	node->key=key;
+	node->value=value;
+	node->left=NULL;
+	node->right=NULL;
+	node->height=1; //heght initialized as 1,newnode will be at height 1
+	
+return (node);
+}
+
+//subtree rooted with y
+struct node *rightRotate(struct node *y)
+{
+	struct node *x=y->left;
+	struct node *T2=x->right;
+	
+	//Do rotations
+	//Just manipulation of pointers so O(1)
+	x->right=y;
+	y->left=T2;
+	
+	//Update heights
+	y->height=max(height(y->left),height(y->right))+1;
+	x->height=max(height(x->left),height(x->right))+1;
+	
+	return x;
+}
+
+//subtree rooted at x
+struct node *leftRotate(struct node *x){
+
+	struct node *y=x->right;
+	struct node *T2=y->left;
+	
+	//Do left rotation
+	y->left = x;
+    	x->right = T2;
+    	
+    	x->height = max(height(x->left), height(x->right))+1;
+    	y->height = max(height(y->left), height(y->right))+1;
+ 
+return y; //y is new root
+
+}
+
+int getBalance(struct node *N)
+{
+    if (N == NULL)
+        return 0;
+    return height(N->left) - height(N->right);
+}
+
+//Insert Function for AVL Trees
+struct node* insert (struct node* node,int key,int value){
+
+	if(node==NULL)
+	return(newNode(key,value));
+	
+	if(key < node->key)
+	   node->left=insert(node->left,key,value);
+	else
+	   node->right=insert(node->right,key,value);
+	   
+	 //Update heights
+	 node->height = max(height(node->left), height(node->right)) + 1;
+ 
+ 	int balance=getBalance(node);
+ 	
+ 	  // Left Left Case
+        if (balance > 1 && key < node->left->key)
+        return rightRotate(node);
+ 
+    	// Right Right Case
+        if (balance < -1 && key > node->right->key)
+        return leftRotate(node);
+ 
+        // Left Right Case
+       if (balance > 1 && key > node->left->key)
+       {
+        node->left =  leftRotate(node->left);
+        return rightRotate(node);
+       }
+ 
+       // Right Left Case
+      if (balance < -1 && key < node->right->key)
+      {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+     }
+     
+     return node;
+
+}
+void printInorder(struct node* node)
+{
+     if (node == NULL)
+          return;
+
+     /* first recur on left child */
+     printInorder(node->left);
+
+     /* then print the key of node */
+     printf("%d %d", node->key,node->value);
+     printf("\n***********\n");  
+
+     /* now recur on right child */
+     printInorder(node->right);
+}
+
+int main()
+{
+ struct node *root=NULL;
+ 
+  root = insert(root,10,1);
+  root = insert(root,20,2);
+  root = insert(root,30,3);
+  root = insert(root,40,4);
+  root = insert(root,50,5);
+  root = insert(root,25,6);
+  
+  printf("Inorder Travelsal of given AVL Tree is\n");
+  printInorder(root); 
+  printf("\n"); 
+ 
+
+return 0;
+}
